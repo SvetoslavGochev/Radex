7d734fcfd87d4f4fed9b24164efa6ee56f9b54f9
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-09-07 19:37:45 +0300

    gg
    Added apiGet apiPost

---
+++ Radex/Controllers/Api/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿namespace Radex.Controllers.Api
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Radex.Data;
+    using System;
+
+    [Route("api/[controller]")]//opennig on name of controller
+    // open on adress /Products za vsi`ki mewtodi pytq e edin i sy6t
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        [HttpGet]
+        public Product Test()
+        {
+            //if (!this.ModelState.IsValid)
+            //{ ako ne6to ne e okei v dannite
+            //    return BadRequest(tModelState);
+            //} tova ne go pravim to e avtomati4no za api kontrolerite
+
+            return new Product
+            {
+                ActiveForm = DateTime.UtcNow,
+                Descriptions = "descroption",
+                Id = 123,
+                Name = "name",
+                Price = 123.34M,
+            };
+            //vra]a go kato jSon
+
+        }
+
+        [HttpDelete]
+        public string SoftUni()
+        {
+            return "Delete!";
+        }
+        [HttpPost]
+        public Product SoftUni2(Product product)
+        {
+            return product;
+        }
+
+    }
+}
--- Radex/Data/Product.cs
+++ Radex/Data/Product.cs
@@ -2,17 +2,21 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Threading.Tasks;
 
     public class Product
     {
         public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
         public string Name { get; set; }
         public string Descriptions { get; set; }
 
         public DateTime ActiveForm { get; set; }
 
+        [Range(0, 10000)]
         public decimal Price { get; set; }
     }
 
