56c4ac70e616891381b5594341340083a8bf9f24
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-09-07 20:11:17 +0300

    gg
    services.AddControllersWithViews().AddXmlSerializerFormatters();

--- Radex/Startup.cs
+++ Radex/Startup.cs
@@ -25,7 +25,7 @@ namespace Radex
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddXmlSerializerFormatters();
             services.AddDbContext<Db>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             
         }
